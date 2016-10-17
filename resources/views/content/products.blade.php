b9da7de4896b70c69bf30188757f249eee71a706
Eldar <eldar.moore@gmail.com>
2016-10-18 01:30:12 +0300

    Showing products in view products file

--- resources/views/content/products.blade.php
+++ resources/views/content/products.blade.php
@@ -6,8 +6,32 @@
         <div class="row">
             <div class="col-md-12">
                 <h1>{{ $cat_title }}</h1>
-                <p>Article demo for about page.</p>
             </div>
         </div>
+    <div class="row">
+
+        @if($products)
+
+            @foreach($products as $row)
+                <div class="col-md-4">
+
+                    @if(strlen($row['title']) > 20)
+
+                    <h2>{{ $row['title'] }}</h2>
+                    @endif
+                    <p><img border="0" width="300" src="{{ asset('/images/' . $row['url'] . '/' . $row['image']) }}" alt=""></p>
+                    <p>{!! $row['article'] !!}</p>
+                    <p><b>Price on site: </b>{{ $row['price'] }}$</p>
+                </div>
+            @endforeach
+
+        @else
+
+            <p><i>No products found...</i></p>
+
+        @endif
+
+
+    </div>
 
 @endsection
