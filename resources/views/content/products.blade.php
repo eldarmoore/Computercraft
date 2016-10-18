6d5b186f918c74da9bdd095cf2a5b6760bb28ff7
Eldar <eldar.moore@gmail.com>
2016-10-18 11:38:31 +0300

    Products display was modified.

--- resources/views/content/products.blade.php
+++ resources/views/content/products.blade.php
@@ -14,21 +14,50 @@
         @if($products)
 
             @foreach($products as $row)
-                <div class="col-md-4">
 
-                    @if(strlen($row['title']) > 20)
+                <div class="col-sm-3 col-lg-3 col-md-3">
+                    <div class="thumbnail">
+                        <img src="{{ asset('/images/' . $row['url'] . '/' . $row['image']) }}" alt=""">
+                        <div class="caption">
 
-                    <h2>{{ $row['title'] }}</h2>
-                    @endif
-                    <p><img border="0" width="300" src="{{ asset('/images/' . $row['url'] . '/' . $row['image']) }}" alt=""></p>
-                    <p>{!! $row['article'] !!}</p>
-                    <p><b>Price on site: </b>{{ $row['price'] }}$</p>
+
+                            @if(strlen($row['title']) > 10)
+
+                            <h4><a href="#">{{ substr($row['title'], 0, 45) . '...' }}</a></h4>
+
+                            @endif
+
+
+
+                            @if(strlen($row['article']) > 10)
+
+                                <p>{{ substr($row['article'], 0, 100) . '...' }}</p>
+
+                            @endif
+
+
+
+                            <p class="pull-right">6 reviews</p>
+                            <p>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star"></span>
+                                <span class="glyphicon glyphicon-star-empty"></span>
+                                <span class="glyphicon glyphicon-star-empty"></span>
+                            </p>
+
+                            <h4 class="pull-right">Price: <b>{{ $row['price'] }}$</b></h4>
+
+                            <span class="clearfix"></span>
+                        </div>
+                    </div>
                 </div>
+
             @endforeach
 
         @else
             <div class="col-md-12">
-            <p><i>No products found...</i></p>
+            <p><i>No products to display...</i></p>
             </div>
         @endif
 
