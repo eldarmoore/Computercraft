7b60344e418a7e0a2b6749ffd782125c83b8e3bc
Eldar <eldar.moore@gmail.com>
2016-10-21 20:46:57 +0300

    carousel modified

--- resources/views/content/home.blade.php
+++ resources/views/content/home.blade.php
@@ -6,11 +6,7 @@
     ================================================== -->
     <div id="myCarousel" class="carousel slide" data-ride="carousel">
         <!-- Indicators -->
-        <ol class="carousel-indicators">
-            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-            <li data-target="#myCarousel" data-slide-to="1"></li>
-            <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>
+        
         <div class="carousel-inner" role="listbox">
             <div class="item active">
                 <img class="first-slide" src="{{ asset('images/Cases-TLC-HeroShot01.jpg') }}" alt="First slide">
