03b3b7d279ab2e50b0a40d6ecd9de6f53653ca7e
Eldar <eldar.moore@gmail.com>
2016-10-24 20:26:17 +0300

    Minor changes for Summernote

--- resources/views/cms/add_content.blade.php
+++ resources/views/cms/add_content.blade.php
@@ -14,7 +14,7 @@
                 </div>
                 <div class="form-group">
                     <label for="article">Article:</label>
-                    <textarea id="summernote" class="form-control" cols="30" rows="10"></textarea>
+                    <textarea id="summernote" class="form-control" rows="10"></textarea>
                 </div>
                 <a href="{{ url('cms/content') }}" class="btn btn-default">Cancel</a>
                 <button type="submit" class="btn btn-primary">Save content</button>
