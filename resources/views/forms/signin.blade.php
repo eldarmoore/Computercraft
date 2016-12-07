41e477f7142cae7ee8ced51ade3a5b63ba36a048
Eldar Moore <eldar.moore@gmail.com>
2016-12-07 16:38:14 +0200

    Facebook integration canceled

--- resources/views/forms/signin.blade.php
+++ resources/views/forms/signin.blade.php
@@ -6,23 +6,6 @@
 
 @section('content')
 
-    <?php
-
-    require base_path('vendor/autoload.php');
-
-    $fb = new Facebook\Facebook([
-            'app_id' => '210744862696224',
-            'app_secret' => '22738f1c32d766a615e91315a076e1b3',
-            'default_graph_version' => 'v2.5',
-    ]);
-
-    $helper = $fb->getRedirectLoginHelper();
-    $permissions = ['email'];
-    $link = 'http://localhost:8000/fb-callback.php';
-    $loginUrl = $helper->getLoginUrl($link, $permissions);
-
-    ?>
-
     <div class="container">
         <form class="form-signin" method="post" style="width: 400px; margin: 0 auto;">
             {{ csrf_field() }} {{-- Token --}}
@@ -40,7 +23,6 @@
                 <hr>
                 <a href="{{ url('user/signup') }}" type="submit" name="submit" class="btn btn-lg btn-default btn-block default-color">Create Account</a>
             </div>
-            <a href="<?= htmlspecialchars($loginUrl); ?>">Log in with Facebook!</a>
         </form>
     </div> <!-- /container -->
 
