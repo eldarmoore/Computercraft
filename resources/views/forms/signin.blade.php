9cff1f37e216e726ff1ba7832a0e358124107583
Eldar Moore <eldar.moore@gmail.com>
2016-12-07 02:27:38 +0200

    Signin and Signup forms has been modified

--- resources/views/forms/signin.blade.php
+++ resources/views/forms/signin.blade.php
@@ -11,11 +11,11 @@
             {{ csrf_field() }} {{-- Token --}}
             <div class="form-group">
                 <label for="email" >Email address</label>
-                <input type="text" name="email" id="inputEmail" class="form-control" placeholder=" enter your email" required autofocus value="{{ Illuminate\Support\Facades\Input::old('email') }}">
+                <input type="text" name="email" id="inputEmail" class="form-control" placeholder="enter your email" required autofocus value="{{ Illuminate\Support\Facades\Input::old('email') }}">
             </div>
             <div class="form-group">
                 <label for="password" >Password</label>
-                <input type="password" name="password" id="inputPassword" class="form-control" placeholder=" enter your password" required>
+                <input type="password" name="password" id="inputPassword" class="form-control" placeholder="enter your password" required>
             </div>
             <div class="form-group">
                 <a href="#">Forgot your password?</a>
--- resources/views/forms/signup.blade.php
+++ resources/views/forms/signup.blade.php
@@ -7,12 +7,12 @@
 @section('content')
 
     <div>
-        <form action="" method="post" class="form-signin">
+        <form action="" method="post">
 
             <div class="row">
 
                 <div class="col-md-12">
-                    <div class="col-md-6">
+                    <div class="col-md-4 col-md-offset-4 form-signin" style="padding-top: 10px;">
                         <div class="form-group">
                             <label for="name">Name:</label>
                             <input type="text" class="form-control" name="name" value="{{ Illuminate\Support\Facades\Input::old('name') }}" placeholder="enter your name" required autofocus>
@@ -22,9 +22,6 @@
                             <label for="email">Email:</label>
                             <input type="text" class="form-control" name="email" value="{{ Illuminate\Support\Facades\Input::old('email') }}" placeholder="enter your email address" required>
                         </div>
-                    </div>
-
-                    <div class="col-md-6">
                         <div class="form-group">
                             <label for="password">Password:</label>
                             <input type="password" class="form-control" name="password" placeholder="enter your password" required>
@@ -33,9 +30,7 @@
                             <label for="password_confirmation">Confirm Password:</label>
                             <input type="password" class="form-control" name="password_confirmation" placeholder="confirm your Password" required>
                         </div>
-                    </div>
 
-                    <div class="col-md-4 col-md-offset-4">
                         <a href="{{ url('user/signin') }}">Or signin with your account?</a>
                         <button type="submit" name="submit" class="btn btn-lg btn-primary btn-block primary-color">Sign up</button>
                     </div>
